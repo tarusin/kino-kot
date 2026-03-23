1353fce9cfd662f4392f12fc603d111db31acef0
tarusin <tarusin.iliya@gmail.com>
2026-03-23 14:34:01 +0200

    changed footer according to the design

--- apps/web/src/components/Footer/Footer.tsx
+++ apps/web/src/components/Footer/Footer.tsx
@@ -1,22 +1,22 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import styles from './Footer.module.scss';
 
 export default function Footer() {
   return (
     <footer className={styles['footer']}>
       <div className={styles['footer__wrap']}>
-        <div className={styles['footer__top']}>
-          <a href="/" className={styles['footer__logo']}>
-            <Image src="/images/logo.svg" alt="КиноКот" width={120} height={33} />
-          </a>
-          <p className={styles['footer__copyright']}>© 2024 Все права защищены KinoKot</p>
+        <Link href="/" className={styles['footer__logo']}>
+          <Image src="/images/logo.svg" alt="КиноКот" width={162} height={44} />
+        </Link>
+        <div className={styles['footer__info']}>
+          <p className={styles['footer__copyright']}>© 2026 · Все права защищены, КиноКот</p>
+          <nav className={styles['footer__nav']}>
+            <Link href="/privacy">Политика Конфиденциальности</Link>
+            <Link href="/licenses">Лицензии</Link>
+            <Link href="/support">Поддержка</Link>
+          </nav>
         </div>
-        <nav className={styles['footer__nav']}>
-          <a href="/films">Фильмы</a>
-          <a href="/series">Сериалы</a>
-          <a href="/cartoons">Мультфильмы</a>
-          <a href="/support">Поддержка</a>
-        </nav>
       </div>
     </footer>
   );
