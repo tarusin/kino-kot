0279e784cc2a707f46a7191df3c173324d9531a3
tarusin <tarusin.iliya@gmail.com>
2026-03-24 16:08:33 +0200

    updated cors

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -9,7 +9,10 @@ async function bootstrap() {
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: [
+      'http://localhost:3000',
+      process.env.FRONTEND_URL,
+    ].filter(Boolean) as string[],
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3001);
